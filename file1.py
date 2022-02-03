6f20cc5fde54777819a920a2cd075022ececc649
manaswiag <97674501+manaswiag@users.noreply.github.com>
2022-02-03 15:31:41 +0530

    CLI commit in file1

---
+++ file1.py
@@ -0,0 +1,10 @@
+import os
+inp = int(input("enter your choice : "))
+if inp==1:
+  os.system("notepad")
+elif inp == 2:
+  os.system("calc")
+elif inp==3:
+  os.system("google chrome")
+ else:
+  print("wrong choice")
